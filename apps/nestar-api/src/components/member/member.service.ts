36238f100b6e838f861ece67c06bd6689c5ee0ca
KhamidkhanM <hoshimalievhamidxonn@gmail.com>
2026-08-13 18:06:50 +0900

    feat: develop signup business logic

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,25 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Member } from '../../libs/dto/member/member';
+import { MemberInput } from '../../libs/dto/member/member.input';
 
 @Injectable()
 export class MemberService {
+	constructor(@InjectModel('Member') private readonly memberModel: Model<Member>) {}
 
-    constructor(@InjectModel('Member') private readonly memberModel: Model<null>) { }
+	public async signup(input: MemberInput): Promise<Member> {
+		// TODO: HASH Password
+		try {
+			const result = await this.memberModel.create(input);
+			// TODO: Auth via TOKEN
+			return result;
+		} catch (err) {
+			console.log('Error, Service.model', err);
+			throw new BadRequestException(err);
+		}
+	}
 
-    public async signup(): Promise<string> {
-        return 'Member signed up successfully';
-    }
+	public async login(): Promise<string> {
+		return 'Member logged in successfully';
+	}
 
-    public async login(): Promise<string> {
-        return 'Member logged in successfully';
-    }
+	public async updateMember(): Promise<string> {
+		return 'Member updated successfully';
+	}
 
-    public async updateMember(): Promise<string> {
-        return 'Member updated successfully';
-    }
-
-    public async getMember(): Promise<string> {
-        return 'Member details retrieved successfully';
-    }
+	public async getMember(): Promise<string> {
+		return 'Member details retrieved successfully';
+	}
 }
