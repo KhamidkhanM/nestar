3097a937fc8b74f526cc4728d9f12bea1adfa467
KhamidkhanM <hoshimalievhamidxonn@gmail.com>
2026-09-05 17:55:04 +0900

    fix: modify getMember business logic

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -7,11 +7,29 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '../auth/auth.module';
 import { ViewModule } from '../view/view.module';
 import { LikeModule } from '../like/like.module';
+import FollowSchema from '../../schemas/Follow.model';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]), AuthModule, ViewModule, LikeModule,
+    MongooseModule.forFeature([{
+      name: 'Member', schema: MemberSchema
+    }
+    ]),
+
+    MongooseModule.forFeature([
+      {
+        name: 'Follow',
+        schema: FollowSchema,
+      },
+    ]),
+
+    AuthModule,
+    ViewModule,
+    LikeModule,
   ],
+
+
+
   providers: [MemberResolver, MemberService],
   exports: [MemberService],
 })
