c7f705cbcb2c309146ec9c9e2a87ce0fb3cb7c64
KhamidkhanM <hoshimalievhamidxonn@gmail.com>
2026-09-08 16:43:35 +0900

    fix: modify like ineraction related logic

--- apps/nestar-api/src/components/follow/follow.service.ts
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -5,7 +5,7 @@ import { Follower, Followers, Following, Followings } from '../../libs/dto/follo
 import { MemberService } from '../member/member.service';
 import { ObjectId } from 'bson';
 import { Direction, Message } from '../../libs/enums/common.enum';
-import { lookupFollowerData, lookupFollowingData } from '../../libs/config';
+import { lookupAuthMemberFollowed, lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
 import { FollowInquiry } from '../../libs/dto/follow/follow.input';
 import { T } from '../../libs/types/common';
 
@@ -74,8 +74,10 @@ export class FollowService {
                         list: [
                             { $skip: (page - 1) * limit },
                             { $limit: limit },
-                            // meLiked
-                            // meFollowed
+                            lookupAuthMemberLiked(memberId, "$followingId"),
+                            lookupAuthMemberFollowed({
+                                followerId: memberId, followingId: '$followingId'
+                            }),
                             lookupFollowingData,
                             { $unwind: '$followingData' },
                         ],
@@ -105,8 +107,10 @@ export class FollowService {
                         list: [
                             { $skip: (page - 1) * limit },
                             { $limit: limit },
-                            // meLiked
-                            // meFollowed
+                            lookupAuthMemberLiked(memberId, "$followerId"),
+                            lookupAuthMemberFollowed({
+                                followerId: memberId, followingId: '$followerId'
+                            }),
                             lookupFollowerData,
                             { $unwind: '$followerData' },
                         ],
